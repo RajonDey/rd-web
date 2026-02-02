787a14ea94af62c2c0bc5fdd26667cc8f35b82a9
Rajon Dey <rajondeyofficial@gmail.com>
2026-02-02 19:55:00 +0600

    updated redirect links for old articles

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: '/2021-in-memory',
+        destination: '/resources/articles/2021-in-memory',
+        permanent: true,
+      },
+      {
+        source: '/2022-in-memory',
+        destination: '/resources/articles/2022-in-memory',
+        permanent: true,
+      },
+      {
+        source: '/2023-in-memory',
+        destination: '/resources/articles/2023-in-memory',
+        permanent: true,
+      },
+      {
+        source: '/2024-in-memory',
+        destination: '/resources/articles/2024-in-memory',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 export default nextConfig;
